3765ebd1e5017f69a90ced602f6fa0d392f41787
luminorena <94684347+luminorena@users.noreply.github.com>
2025-02-05 01:20:30 +0300

    deleted comments and added README

--- src/main/java/ru/otus/java/pro/spring/app/SpringAppApplication.java
+++ src/main/java/ru/otus/java/pro/spring/app/SpringAppApplication.java
@@ -8,18 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringAppApplication implements CommandLineRunner {
-	/*
-+	1. Добавьте сущность СчетКлиента/Account (id PK, номерСчета, клиентИд, баланс, заблокированЛи)
-+	2. Клиент может запросить информацию по счетам/счету(поиск по ИД)
-+	3. Доделайте процесс перевода, деньги должны быть списаны с одного счета и зачислены на другой, добавьте
-	все необходимые, как вам кажется, проверки (+само собой в БД должен быть создан новый Transfer)
-	проверки: деньги есть на счете, счет не заблокирован
-+	4. Добавьте обработку BusinessLogicException (оно должно оборачиваться в 400)
-	клиент ввел счет, пвтается перевести деньги, а денег меньше на счету, закончились лимиты, счет заблокирован у получателя
-	5. Метод GET /api/v1/transfers должен возвращать не только переводы, где клиент является инициатором, но
-	и где он является получателем
-	 */
-
 
     private static final Logger logger = LoggerFactory.getLogger(SpringAppApplication.class.getName());
 
